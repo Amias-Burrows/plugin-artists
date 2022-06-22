47699f0b16c7623d0857399dca7ca7e99dae48c2
amias-burrows <amias.burrows@gmail.com>
2022-06-22 19:26:41 +0100

    testing it so far

--- artists-masonry.php
+++ artists-masonry.php
@@ -12,21 +12,49 @@
  */
 
 
-	defined('ABSPATH' or die("This file is not accessable directly."));
+defined('ABSPATH') or die("This file is not accessable directly.");
 
-	class artistsMasonry {
-		function styles() {
-			wp_enqueue_style('artists-masonry', plugins_url('assets/css/masonry.css', __FILE__), array(), null, 'all');
-		}
-		function register_block() {
-			register_block_type(__DIR__ . 'artists-masonry');
-		}
+class artistsMasonry {
+	function __contruct() {
 	}
 
-	if (class_exists('artistsMasonry')) {
-		$artistsMasonry = new artistsMasonry();
+	function styles() {
+		wp_enqueue_style('artists-masonry', plugins_url('assets/css/masonry.css', __FILE__), array(), null, 'all');
 	}
 
-	add_action('wp_enqueue_scripts', array($artistsMasonry, 'styles'));
-	add_action('init', array($artistsMasonry, 'register_block'));
+	function settings() {
+		add_options_page('Homepage Menu', 'AR Burrows Homepage Menu', 'manage_options', 'ar_burrows_homepage_menu', array($artistsMasonry, 'menu_contents') );
+	}
+
+	function menu_contents() {?>
+		<p>This is the plugin contents</p>
+	<?php
+	}
+
+	function register_block() {
+		register_block_type(__DIR__ . 'artists-masonry');
+	}
+
+	function activate() {
+		flush_rewrite_rules();
+	}
+
+	function deactivate() {
+		flush_rewrite_rules();
+	}
+}
+
+if (class_exists('artistsMasonry')) {
+	$artistsMasonry = new artistsMasonry();
+}
+	//Activation
+register_activation_hook( __FILE__, array($artistsMasonry, 'activate'));
+//Deactivation
+register_deactivation_hook( __FILE__, array( $artistsMasonry, 'deactivate'));
+//Uninstall
+register_uninstall_hook( __FILE__, array( $artistsMasonry, 'uninstall'));
+
+add_action('wp_enqueue_scripts', array($artistsMasonry, 'styles'));
+add_action('admin_menu', array($artistsMasonry . 'settings'));
+add_action('init', array($artistsMasonry, 'register_block'));
 ?>
---
+++ uninstall.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * trigger this file on uninstall 
+ *
+ * @package ArtistsMasonry
+ */
+
+defined('WP_UNINSTALL_PLUGIN') or die("This file is not accessable directly.");
+
+// Clear database stored data
+
+
