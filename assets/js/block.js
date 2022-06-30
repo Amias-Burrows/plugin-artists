ff15f68f33285455361cc052778a087e9b858346
amias-burrows <amias.burrows@gmail.com>
2022-07-01 00:07:07 +0100

    Found a few flaws in my plan
    I will need to do more research if I'm hoping to be able to get this
    done on time

--- assets/js/block.js
+++ assets/js/block.js
@@ -1,11 +1,28 @@
-wp.blocks.registerBlockType('artists-masonry/custom-block', {
+let block = function(title, description, image, perma) {	// Attempt at object function
+	let title[] = title;
+	let description[] = description;
+	let image[] = image;
+	let perma[] = perma;
+	return null;
+}
+wp.blocks.registerBlockType('artists-masonry/custom-block', {	// The actual Gutenberg block
 	title: 'Artists Attic Masonry Grid',
 	icon: 'grid-view',
 	category: 'design',
-	edit: function(props) {
-		return React.createElement('p', null, 'Hello World (From the Editor)');
+	attributes: {	// Used as the props in the React code in edit:
+		object: { type: 'block' },
+		other: { type: 'array' }
+	}
+// Need a Title and Description of page and an image to show with a link to the page as well
 	},
-	save: function(props) {
+	edit: function(props) {	// What shows up in the editor
+		return {
+			/*__PURE__*/React.createElement('div', null, {
+				React.createElement('input', null)
+			});
+		};
+	},
+	save: function(props) {	//What shows up on the frontend
 		return react.createElement('p', null, 'Hola Mundo (From the Frontend)');
 	}
 });
